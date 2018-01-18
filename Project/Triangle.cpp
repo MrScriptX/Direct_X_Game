3e16d71e8ee916c456dda68ea9d04abd91ef45e4
MrScriptX <scriptx001@gmail.com>
2018-01-18 23:20:02 +0100

    added States

--- Project/Triangle.cpp
+++ Project/Triangle.cpp
@@ -12,6 +12,7 @@ Triangle::Triangle(Renderer& renderer)
 {
 	createMesh(renderer);
 	createShader(renderer);
+	createRenderStates(renderer);
 }
 
 
@@ -21,10 +22,17 @@ Triangle::~Triangle()
 	m_vertexShader->Release();
 	m_pixelShader->Release();
 	m_inputLayout->Release();
+	m_rasterizerState->Release();
+	m_depthState->Release();
+	m_blendState->Release();
 }
 
 void Triangle::draw(Renderer & renderer)
 {
+	renderer.getDeviceContext()->RSSetState(m_rasterizerState);
+	renderer.getDeviceContext()->OMSetBlendState(m_blendState, NULL, 0xffffffff);
+	renderer.getDeviceContext()->OMSetDepthStencilState(m_depthState, 1);
+
 	//Bind Triangle shader
 	renderer.getDeviceContext()->IASetInputLayout(m_inputLayout);
 	renderer.getDeviceContext()->VSSetShader(m_vertexShader, nullptr, 0);
@@ -34,8 +42,8 @@ void Triangle::draw(Renderer & renderer)
 	UINT stride = sizeof(Vertex);//size between each vertex
 	UINT offset = 0;
 
+	renderer.getDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_POINTLIST);
 	renderer.getDeviceContext()->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
-
 	renderer.getDeviceContext()->Draw(3, 0);
 }
 
@@ -52,10 +60,10 @@ void Triangle::createMesh(Renderer & renderer)
 
 void Triangle::createShader(Renderer & renderer)
 {
-	std::ifstream vsFile("C:\\Users\\scrip\\Source\\Repos\\Proj_direct\\Project\\VertexShader.cso", std::ios::binary);
+	std::ifstream vsFile("VertexShader.cso", std::ios::binary);
 	std::vector<char> vsData = { std::istreambuf_iterator<char>(vsFile), std::istreambuf_iterator<char>() };
 
-	std::ifstream psFile("C:\\Users\\scrip\\Source\\Repos\\Proj_direct\\Project\\PixelShader.cso", std::ios::binary);
+	std::ifstream psFile("PixelShader.cso", std::ios::binary);
 	std::vector<char> psData = { std::istreambuf_iterator<char>(psFile), std::istreambuf_iterator<char>() };
 
 	renderer.getDevice()->CreateVertexShader(vsData.data(), vsData.size(), nullptr, &m_vertexShader);
@@ -66,6 +74,27 @@ void Triangle::createShader(Renderer & renderer)
 							{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 } };
 
 	renderer.getDevice()->CreateInputLayout(layout, 2, vsData.data(), vsData.size(), &m_inputLayout);
+}
 
+void Triangle::createRenderStates(Renderer& renderer) {
+	// Rasterizer state
+	auto rasterizerDesc = CD3D11_RASTERIZER_DESC(
+		D3D11_FILL_SOLID,
+		D3D11_CULL_NONE,
+		false,
+		0, 0, 0, 0,
+		false, false, false);
+	renderer.getDevice()->CreateRasterizerState(&rasterizerDesc, &m_rasterizerState);
 
+	// Blend state
+	auto blendDesc = CD3D11_BLEND_DESC(CD3D11_DEFAULT());
+	renderer.getDevice()->CreateBlendState(&blendDesc, &m_blendState);
+
+	// Depth state
+	auto depthDesc = CD3D11_DEPTH_STENCIL_DESC(
+		FALSE, D3D11_DEPTH_WRITE_MASK_ZERO, D3D11_COMPARISON_LESS,
+		FALSE, D3D11_DEFAULT_STENCIL_READ_MASK, D3D11_DEFAULT_STENCIL_WRITE_MASK,
+		D3D11_STENCIL_OP_KEEP, D3D11_STENCIL_OP_KEEP, D3D11_STENCIL_OP_KEEP, D3D11_COMPARISON_ALWAYS,
+		D3D11_STENCIL_OP_KEEP, D3D11_STENCIL_OP_KEEP, D3D11_STENCIL_OP_KEEP, D3D11_COMPARISON_ALWAYS);
+	renderer.getDevice()->CreateDepthStencilState(&depthDesc, &m_depthState);
 }
--- Project/Triangle.h
+++ Project/Triangle.h
@@ -13,10 +13,14 @@ public:
 private:
 	void createMesh(Renderer& renderer);
 	void createShader(Renderer& renderer);
+	void createRenderStates(Renderer& renderer);
 
 	ID3D11Buffer * m_vertexBuffer = nullptr;
 	ID3D11VertexShader * m_vertexShader = nullptr;
 	ID3D11PixelShader * m_pixelShader = nullptr;
 	ID3D11InputLayout * m_inputLayout = nullptr;
+	ID3D11RasterizerState* m_rasterizerState = nullptr;
+	ID3D11DepthStencilState* m_depthState = nullptr;
+	ID3D11BlendState* m_blendState = nullptr;
 };
 
